6ad4e587f9c95bdc86370861fc06a5f511c74a6f
BrainBlasted <brainblastedmods@gmail.com>
2017-09-14 16:09:17 -0400

    Moved code for Health to Health.java

--- src/brainblasted/JavaAdventure/Properties/Health.java
+++ src/brainblasted/JavaAdventure/Properties/Health.java
@@ -7,9 +7,18 @@ import brainblasted.JavaAdventure.Properties.ClassLibrary.EntityClass;
  * Health
  */
 public class Health {
-
-    public static double EntityHealth (EntityClass classOfEntity) {
-        double health = ClassLibrary.ClassHealthLibrary(classOfEntity);
+    
+    // Provides the health of given entityClass
+    public static double EntityHealth (EntityClass entityClass) {
+        double health = 0;
+        final double BASE_HEALTH = 100;
+        if (entityClass == EntityClass.SpiderClass) {
+            health = BASE_HEALTH + (0.5 * BASE_HEALTH);
+        } else if (entityClass == EntityClass.PlayerClass) {
+            health = BASE_HEALTH;
+        } else if (entityClass == EntityClass.GiantClass) {
+            health = BASE_HEALTH + (0.75 * BASE_HEALTH);
+        }
         return health;
     }
 }
